fdad3505500bc5be5bae92943fb8b0adb093fa79
Andrew Cloete <andrewhcloete@gmail.com>
2023-09-28 08:42:24 +0200

    Adds secrets.h

---
+++ secrets.example.h
@@ -0,0 +1,6 @@
+#define MQTT_USER "mqtt-user"
+#define MQTT_PASSWORD "mqtt-psw"
+
+const char *ssid = "PrettyFlyForAWifi";
+const char *password = "wifi-psw";
+const char *mqtt_server = "homeassistant";
